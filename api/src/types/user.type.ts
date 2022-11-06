37f56612a55814763b135d98ee30b813ad58fdb3
Fadhil <fadhilsaheer@gmail.com>
2022-11-06 11:47:06 +0530

    update user args

--- api/src/types/user.type.ts
+++ api/src/types/user.type.ts
@@ -9,3 +9,11 @@ export type UserLoginArgs = {
   email: string;
   password: string;
 };
+
+// * all fields starts with `new` will have default data of user if he didn't update any of fields
+export interface UserUpdateArgs {
+  newName?: string;
+  newEmail?: string;
+  newProfile?: string;
+  newPassword?: string;
+}
