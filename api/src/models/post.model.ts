cce2c83e45c1b4f16b5a9945757d689701186716
Fadhil <fadhilsaheer@gmail.com>
2022-11-10 19:36:19 +0530

    delete post model

--- api/src/models/post.model.ts
+++ api/src/models/post.model.ts
@@ -11,3 +11,17 @@ export const createPost = (data: CreatePostArgs) => {
       .catch(reject);
   });
 };
+
+// TODO: add authentication
+export const deletePost = (postId: number) => {
+  return new Promise((resolve, reject) => {
+    prismaClient.post
+      .delete({
+        where: {
+          id: postId,
+        },
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+};
