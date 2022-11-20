4fa448937fb0c6a3c23bef3ce9d4c581ba9b2d7d
Fadhil <fadhilsaheer@gmail.com>
2022-11-20 17:42:18 +0530

    returning generated token with the data

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,27 +1,26 @@
 import { GraphQLError } from 'graphql';
 import { registerUser, loginUser } from '@/models/user.model';
 import { UserLoginArgs, UserRegisterArgs } from '@/types/user.type';
+import { generateToken } from '@/utils/jwt';
 
 export const registerController = async (data: UserRegisterArgs) => {
   const user: any = await registerUser(data).catch((err) => {
-    throw new GraphQLError(err?.message, {
-      extensions: {
-        code: 'UserInputError',
-      },
-    });
+    throw new GraphQLError(err);
   });
 
-  return user;
+  return {
+    ...user,
+    token: generateToken(user),
+  };
 };
 
 export const loginController = async (data: UserLoginArgs) => {
   const user: any = await loginUser(data).catch((err) => {
-    throw new GraphQLError(err?.message, {
-      extensions: {
-        code: 'UserInputError',
-      },
-    });
+    throw new GraphQLError(err);
   });
 
-  return user;
+  return {
+    ...user,
+    token: generateToken(user),
+  };
 };
