f3dfe1de50ebb2fb91fcc39ecaf1d0d13b4c473e
Fadhil <fadhilsaheer@gmail.com>
2022-11-22 07:07:59 +0530

    update ngo controller

--- api/src/controllers/ngo.controller.ts
+++ api/src/controllers/ngo.controller.ts
@@ -1,12 +1,32 @@
-import { createNGO } from '@/models/ngo.model';
-import { CreateNGOArgs } from '@/types/ngo.type';
+import { authenticateNGOAdmin, createNGO, updateNGO } from '@/models/ngo.model';
+import { CreateNGOArgs, UpdateNGOArgs } from '@/types/ngo.type';
 import { GraphQLError } from 'graphql';
 
 export const createNgoController = async (
   userId: number,
   data: CreateNGOArgs
 ) => {
-  return await createNGO(userId, data).catch(() => {
-    throw new GraphQLError('Failed to create NGO');
+  return await createNGO(userId, data).catch((err) => {
+    throw new GraphQLError(err);
+  });
+};
+
+export const updateNgoController = async (
+  userId: number,
+  ngoId: string,
+  data: UpdateNGOArgs
+) => {
+  let authentication = await authenticateNGOAdmin(userId, ngoId).catch(
+    (err) => {
+      throw new GraphQLError(err);
+    }
+  );
+
+  if (!authentication) {
+    throw new GraphQLError('Permission denied');
+  }
+
+  return await updateNGO(ngoId, data).catch((err) => {
+    throw new GraphQLError(err);
   });
 };
