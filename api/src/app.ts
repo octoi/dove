f525bcc7c9410584ee4872381df4b509aba5e15a
Fadhil <fadhilsaheer@gmail.com>
2022-12-13 08:51:01 +0530

    file server

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
+import multer from 'multer';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { schema } from './graphql';
@@ -10,6 +11,26 @@ const httpServer = http.createServer(app);
 const graphqlServer = new ApolloServer({ schema });
 
 const PORT = Number(process.env.PORT) || 5000;
+
+// configure file api
+
+let storage = multer.diskStorage({
+  destination: (_, __, cb) => cb(null, 'uploads/'),
+  filename: (_, file, cb) => {
+    let extArray = file.mimetype.split('/');
+    let extension = extArray[extArray.length - 1];
+    cb(null, Date.now() + '.' + extension);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use('/file', express.static('uploads')); // serving uploaded files to `file/${filename}`
+
+app.post('/file', upload.single('uploaded_file'), (req, res) => {
+  let path = `${req.file?.filename}`;
+  res.json(path);
+});
 
 const startServer = async () => {
   await graphqlServer.start();
