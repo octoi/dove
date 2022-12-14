5dd61c46cb815281587f91e45d7d26c38ca27310
Fadhil <fadhilsaheer@gmail.com>
2022-12-14 21:51:01 +0530

    file api endpoint

--- app/utils/constant.ts
+++ app/utils/constant.ts
@@ -1,1 +1,2 @@
-export const GRAPHQL_ENDPOINT = 'http://localhost:5000';
+export const GRAPHQL_ENDPOINT = 'http://localhost:5000/graphql';
+export const FILE_API_ENDPOINT = 'http://localhost:5000/file';
