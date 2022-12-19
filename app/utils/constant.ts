c043c39de002ca423e80e19072d56e97a5369e59
Fadhil <fadhilsaheer@gmail.com>
2022-12-19 22:19:30 +0530

    getServerSideProps 404 constant

--- app/utils/constant.ts
+++ app/utils/constant.ts
@@ -1,2 +1,13 @@
+import { Paths } from './paths';
+
 export const GRAPHQL_ENDPOINT = 'http://localhost:5000/graphql';
 export const FILE_API_ENDPOINT = 'http://localhost:5000/file';
+
+// getServerSideProps redirect to 404 data
+export const GSSPRedirectData = {
+  redirect: {
+    destination: Paths.notFound,
+    permanent: false,
+  },
+  props: {},
+};
