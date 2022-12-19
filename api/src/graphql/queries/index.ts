7dea0311f4b5595dbb03e10f3e39966b2c99fa7c
Fadhil <fadhilsaheer@gmail.com>
2022-12-19 22:17:42 +0530

    get ngo query

--- api/src/graphql/queries/index.ts
+++ api/src/graphql/queries/index.ts
@@ -1,13 +1,9 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLObjectType } from 'graphql';
+import { GetNgoDetailsQuery } from './ngo.query';
 
 export const Queries = new GraphQLObjectType({
   name: 'Queries',
   fields: () => ({
-    hello: {
-      type: GraphQLString,
-      resolve: () => {
-        return 'hello world';
-      },
-    },
+    getNgoDetails: GetNgoDetailsQuery,
   }),
 });
