7174db6e463e138ae57a134730ddc5ae0135014b
Fadhil <fadhilsaheer@gmail.com>
2022-12-26 22:42:52 +0530

    dismiss ngo admin controller

--- api/src/controllers/ngo.controller.ts
+++ api/src/controllers/ngo.controller.ts
@@ -8,6 +8,7 @@ import {
   makeNGOAdmin,
   updateNGO,
   getNGODetails,
+  dismissNGOAdmin,
 } from '@/models/ngo.model';
 
 export const createNgoController = async (
@@ -77,6 +78,24 @@ export const makeNgoAdminController = async (
   }
 
   return await makeNGOAdmin(ngoId, targetUserId);
+};
+
+export const dismissNgoAdminController = async (
+  requestUserId: number,
+  targetUserId: number,
+  ngoId: string
+) => {
+  let authentication = await authenticateNGOAdmin(requestUserId, ngoId).catch(
+    (err) => {
+      throw new GraphQLError(err);
+    }
+  );
+
+  if (!authentication) {
+    throw new GraphQLError('Permission denied');
+  }
+
+  return await dismissNGOAdmin(ngoId, targetUserId);
 };
 
 export const getNgoController = async (ngoId: string) => {
