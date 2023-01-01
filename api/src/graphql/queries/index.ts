c7d3e893b5c345aff1cb779d0b33b98477277c26
Fadhil <fadhilsaheer@gmail.com>
2023-01-01 08:24:32 +0530

    api: get post query

--- api/src/graphql/queries/index.ts
+++ api/src/graphql/queries/index.ts
@@ -1,9 +1,12 @@
 import { GraphQLObjectType } from 'graphql';
 import { GetNgoDetailsQuery } from './ngo.query';
+import { GetPostQuery } from './post.query';
 
 export const Queries = new GraphQLObjectType({
   name: 'Queries',
   fields: () => ({
     getNgoDetails: GetNgoDetailsQuery,
+    // post query
+    getPost: GetPostQuery,
   }),
 });
